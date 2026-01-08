76647e54e80667d3f330dd3939b4a14f7a3f6ee3
wangyuzhan <wangyuzhan@bytedance.com>
2026-01-08 16:57:03 +0800

    revert decode kv cache offload free

--- python/sglang/srt/disaggregation/decode_kvcache_offload_manager.py
+++ python/sglang/srt/disaggregation/decode_kvcache_offload_manager.py
@@ -116,15 +116,9 @@ class DecodeKVCacheOffloadManager:
         incremental_tokens = all_tokens[start:end]
         incremental_indices = token_indices[start:end]
 
-        # Early free prefill-offloaded GPU memory (NSA-aware)
+        # Early free prefill-offloaded GPU memory
         if prefill_offloaded_len > 0:
-            if enable_nsa_hybrid_indexer_pool(req_to_token_pool=self.req_to_token_pool):
-                indices = self.req_to_token_pool.get_all_indices_range(
-                    req.req_pool_idx, 0, prefill_offloaded_len
-                )
-            else:
-                indices = token_indices[:prefill_offloaded_len]
-            self.token_to_kv_pool_allocator.free(indices)
+            self.token_to_kv_pool_allocator.free(token_indices[:prefill_offloaded_len])
 
         # Asynchronously offload incremental KV cache from device to host
         self.request_counter += 1
@@ -191,13 +185,11 @@ class DecodeKVCacheOffloadManager:
 
     def _release_finished_req(self, req: Req, prefill_offloaded_len: int):
         kv_committed_len = req.pop_committed_kv_cache()
-        # Free the incremental part of the request (NSA-aware)
-        if enable_nsa_hybrid_indexer_pool(req_to_token_pool=self.req_to_token_pool):
-            kv_indices = self.req_to_token_pool.get_all_indices_range(
-                req.req_pool_idx, prefill_offloaded_len, kv_committed_len
-            )
-        else:
-            kv_indices = self.req_to_token_pool.req_to_token[req.req_pool_idx, prefill_offloaded_len:kv_committed_len]
+        kv_indices = self.req_to_token_pool.req_to_token[
+            req.req_pool_idx, prefill_offloaded_len:kv_committed_len
+        ]
+
+        # Free the incremental part of the request
         self.token_to_kv_pool_allocator.free(kv_indices)
         
         self.req_to_token_pool.free(req.req_pool_idx)
