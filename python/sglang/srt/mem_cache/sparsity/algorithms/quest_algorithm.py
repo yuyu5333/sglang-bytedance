ee680795bed6d110dad3f8884d1e2a53b6e9086e
wangyuzhan <wangyuzhan@bytedance.com>
2026-01-20 17:27:32 +0800

    debug info quest

--- python/sglang/srt/mem_cache/sparsity/algorithms/quest_algorithm.py
+++ python/sglang/srt/mem_cache/sparsity/algorithms/quest_algorithm.py
@@ -185,7 +185,7 @@ class QuestAlgorithm(BaseSparseAlgorithmImpl):
 
         if not valid_mask.any():
             return
-
+        print(f"[DEBUG] [construct_representations] run _compute_page_representations")
         # Compute page representations by subclass
         self._compute_page_representations(
             layer_id,
@@ -223,6 +223,7 @@ class QuestAlgorithm(BaseSparseAlgorithmImpl):
         if not valid_mask.any():
             return
 
+        print(f"[DEBUG] [update_representations] run _compute_page_representations")    
         # Compute page representations by subclass
         self._compute_page_representations(
             layer_id,
