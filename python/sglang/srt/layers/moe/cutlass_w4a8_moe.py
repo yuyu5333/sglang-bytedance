0d157ecf32517e787ad40a71b70f6bbec841c40d
zekai <zekai01@antgroup.com>
2026-04-08 00:16:50 -0700

    perf(w4a8_moe): reduce decode overhead with fused FP8 scale kernel and cleanup && quick fix a2 quant scale problem

--- python/sglang/srt/layers/moe/cutlass_w4a8_moe.py
+++ python/sglang/srt/layers/moe/cutlass_w4a8_moe.py
@@ -6,6 +6,20 @@ import torch
 from sgl_kernel import cutlass_w4a8_moe_mm, get_cutlass_w4a8_moe_mm_data, silu_and_mul
 
 from sglang.jit_kernel.per_tensor_quant_fp8 import per_tensor_quant_fp8
+
+from sglang.srt.utils import is_cuda_alike
+
+_is_cuda_alike = is_cuda_alike()
+
+if _is_cuda_alike:
+    from sgl_kernel import (
+        cutlass_w4a8_moe_mm,
+        get_cutlass_w4a8_moe_mm_data,
+        silu_and_mul,
+    )
+    from sglang.jit_kernel.per_tensor_absmax_fp8 import per_tensor_absmax_fp8
+    from sglang.jit_kernel.per_tensor_quant_fp8 import per_tensor_quant_fp8
+
 from sglang.srt.distributed import get_moe_expert_parallel_world_size
 from sglang.srt.layers.moe.ep_moe.kernels import (
     cutlass_w4_run_moe_ep_preproess,
@@ -16,6 +30,7 @@ from sglang.srt.layers.moe.ep_moe.kernels import (
     post_reorder_for_cutlass_moe,
     pre_reorder_for_cutlass_moe,
     silu_and_mul_masked_post_per_tensor_quant_fwd,
+    silu_mul_dynamic_tensorwise_quant_for_cutlass_moe,
     silu_mul_static_tensorwise_quant_for_cutlass_moe,
 )
 
@@ -120,13 +135,10 @@ def cutlass_w4a8_moe(
         dtype=torch.float8_e4m3fn,
     )
 
+    # TODO: fuse per_tensor_absmax_fp8 and pre_reorder_for_cutlass_moe
     if a1_scale is None:
-        a1_scale = (
-            torch.max(torch.abs(a))
-            .to(torch.float32)
-            .div_(torch.finfo(torch.float8_e4m3fn).max)
-            .view(1)
-        )
+        a1_scale = torch.zeros(1, dtype=torch.float32, device=device)
+        per_tensor_absmax_fp8(a, a1_scale)
 
     pre_reorder_for_cutlass_moe(
         a,
@@ -181,16 +193,14 @@ def cutlass_w4a8_moe(
     )
 
     if a2_scale is None:
-        a2_scale = (
-            torch.max(torch.abs(c1))
-            .to(torch.float32)
-            .div_(torch.finfo(torch.float8_e4m3fn).max)
-            .view(1)
+        a2_scale = torch.zeros(1, dtype=torch.float32, device=device)
+        silu_mul_dynamic_tensorwise_quant_for_cutlass_moe(
+            c1, intermediate_q, a2_scale, expert_offsets[-1:], m * topk, n
         )
-
-    silu_mul_static_tensorwise_quant_for_cutlass_moe(
-        c1, intermediate_q, a2_scale.float(), expert_offsets[-1:], m * topk, n
-    )
+    else:
+        silu_mul_static_tensorwise_quant_for_cutlass_moe(
+            c1, intermediate_q, a2_scale.float(), expert_offsets[-1:], m * topk, n
+        )
 
     cutlass_w4a8_moe_mm(
         c2,
