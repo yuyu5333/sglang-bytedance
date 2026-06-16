f0d7f244a696305b2daa49a110aef9c81b9c0a0d
wangyuzhan <wangyuzhan@bytedance.com>
2026-06-16 11:12:18 +0800

    format code

--- python/sglang/srt/layers/moe/cutlass_w4a8_moe.py
+++ python/sglang/srt/layers/moe/cutlass_w4a8_moe.py
@@ -22,7 +22,10 @@ if _is_cuda:
 else:
     from sgl_kernel import silu_and_mul
 
-from sglang.jit_kernel.per_tensor_quant_fp8 import per_tensor_quant_fp8, per_tensor_absmax_fp8
+from sglang.jit_kernel.per_tensor_quant_fp8 import (
+    per_tensor_absmax_fp8,
+    per_tensor_quant_fp8,
+)
 
 from sglang.srt.layers.moe.ep_moe.kernels import (
     cutlass_w4_run_moe_ep_preproess,
