8d9bcb3d3e96cda79a9b9041dc359de20f8bed44
wangyuzhan <wangyuzhan@bytedance.com>
2026-07-02 16:17:22 +0800

    [flashmla-kv2bit] bump FlashMLA to e4b77aa (Route G step8: sX double-buffer)

--- sgl-kernel/cmake/flashmla.cmake
+++ sgl-kernel/cmake/flashmla.cmake
@@ -106,7 +106,7 @@ include(FetchContent)
 FetchContent_Declare(
     repo-flashmla
     GIT_REPOSITORY https://github.com/yuyu5333/FlashMLA
-    GIT_TAG f7c5185
+    GIT_TAG e4b77aa
     GIT_SHALLOW OFF
 )
 FetchContent_Populate(repo-flashmla)
