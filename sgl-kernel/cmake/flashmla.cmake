11ec00180572344031b80d87bf14d53ca21c6cf1
wangyuzhan <wangyuzhan@bytedance.com>
2026-07-02 16:47:26 +0800

    [flashmla-kv2bit] bump FlashMLA to 03e9dad (Route G step9 dir-A wgmma pipeline)

--- sgl-kernel/cmake/flashmla.cmake
+++ sgl-kernel/cmake/flashmla.cmake
@@ -106,7 +106,7 @@ include(FetchContent)
 FetchContent_Declare(
     repo-flashmla
     GIT_REPOSITORY https://github.com/yuyu5333/FlashMLA
-    GIT_TAG e4b77aa
+    GIT_TAG 03e9dad
     GIT_SHALLOW OFF
 )
 FetchContent_Populate(repo-flashmla)
