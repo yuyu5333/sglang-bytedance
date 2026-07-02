e11a11f146d75f1dc36eaef5c119fc0249b8c37b
wangyuzhan <wangyuzhan@bytedance.com>
2026-07-02 22:41:24 +0800

    [flashmla-kv2bit] bump FlashMLA to 118949a (async wgmma + fill overlap)

--- sgl-kernel/cmake/flashmla.cmake
+++ sgl-kernel/cmake/flashmla.cmake
@@ -106,7 +106,7 @@ include(FetchContent)
 FetchContent_Declare(
     repo-flashmla
     GIT_REPOSITORY https://github.com/yuyu5333/FlashMLA
-    GIT_TAG b398760
+    GIT_TAG 118949a
     GIT_SHALLOW OFF
 )
 FetchContent_Populate(repo-flashmla)
