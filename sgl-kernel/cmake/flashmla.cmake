5399e7d1cd9835e601f3bdb901e2d9e29c70ca88
wangyuzhan <wangyuzhan@bytedance.com>
2026-07-03 12:04:24 +0800

    [M3.c] opt: update FlashMLA to ab5b76b (QK split-K across WG0+WG1)

--- sgl-kernel/cmake/flashmla.cmake
+++ sgl-kernel/cmake/flashmla.cmake
@@ -106,7 +106,7 @@ include(FetchContent)
 FetchContent_Declare(
     repo-flashmla
     GIT_REPOSITORY https://github.com/yuyu5333/FlashMLA
-    GIT_TAG 3fd2816
+    GIT_TAG ab5b76b
     GIT_SHALLOW OFF
 )
 FetchContent_Populate(repo-flashmla)
