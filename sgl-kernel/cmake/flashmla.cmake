1128cb57fe073d05baf8f093ab16f17f34495aed
wangyuzhan <wangyuzhan@bytedance.com>
2026-07-03 16:24:18 +0800

    [M3.c] pin FlashMLA back to 3fd2816 for exact step14 replay

--- sgl-kernel/cmake/flashmla.cmake
+++ sgl-kernel/cmake/flashmla.cmake
@@ -106,7 +106,7 @@ include(FetchContent)
 FetchContent_Declare(
     repo-flashmla
     GIT_REPOSITORY https://github.com/yuyu5333/FlashMLA
-    GIT_TAG 56c95fe
+    GIT_TAG 3fd2816
     GIT_SHALLOW OFF
 )
 FetchContent_Populate(repo-flashmla)
