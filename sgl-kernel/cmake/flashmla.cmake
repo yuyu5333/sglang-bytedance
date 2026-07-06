81c51dec04d2c89a1c531006aa28c83742798d7f
wangyuzhan <wangyuzhan@bytedance.com>
2026-07-06 23:36:33 +0800

    [flashmla-kv2bit] bump FlashMLA tag to dfe5666 (clk linkage fix)

--- sgl-kernel/cmake/flashmla.cmake
+++ sgl-kernel/cmake/flashmla.cmake
@@ -106,7 +106,7 @@ include(FetchContent)
 FetchContent_Declare(
     repo-flashmla
     GIT_REPOSITORY https://github.com/yuyu5333/FlashMLA
-    GIT_TAG 9adac11
+    GIT_TAG dfe5666
     GIT_SHALLOW OFF
 )
 FetchContent_Populate(repo-flashmla)
