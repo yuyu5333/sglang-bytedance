8cef5246aa7c75d7df2a98581a6982f43b107c12
wangyuzhan <wangyuzhan@bytedance.com>
2026-07-06 18:00:43 +0800

    [fmla-dequant] bump FlashMLA pin to 733578f (Route H step2a probe disabled, byte-correct default)

--- sgl-kernel/cmake/flashmla.cmake
+++ sgl-kernel/cmake/flashmla.cmake
@@ -106,7 +106,7 @@ include(FetchContent)
 FetchContent_Declare(
     repo-flashmla
     GIT_REPOSITORY https://github.com/yuyu5333/FlashMLA
-    GIT_TAG 9ea2a58
+    GIT_TAG 733578f
     GIT_SHALLOW OFF
 )
 FetchContent_Populate(repo-flashmla)
