578cda9426339f2f8da6ec827d5a69c9336fc26c
wangyuzhan <wangyuzhan@bytedance.com>
2026-07-15 10:56:24 +0800

    [flashmla-kv2bit] bump FlashMLA pin to 2a2d8ef (step5a fill_sX null probe)

--- sgl-kernel/cmake/flashmla.cmake
+++ sgl-kernel/cmake/flashmla.cmake
@@ -125,7 +125,7 @@ include(FetchContent)
 FetchContent_Declare(
     repo-flashmla
     GIT_REPOSITORY https://github.com/yuyu5333/FlashMLA
-    GIT_TAG 809f259
+    GIT_TAG 2a2d8ef
     GIT_SHALLOW OFF
 )
 FetchContent_Populate(repo-flashmla)
