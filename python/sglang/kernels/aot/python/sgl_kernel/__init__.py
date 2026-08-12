4983448a7693880fafaa40e71e1aa8dbb786b026
wangyuzhan <wangyuzhan@bytedance.com>
2026-08-12 14:55:55 +0800

    feat: add mxfp4afp8 cutlass

--- python/sglang/kernels/aot/python/sgl_kernel/__init__.py
+++ python/sglang/kernels/aot/python/sgl_kernel/__init__.py
@@ -25,6 +25,7 @@ else:
     from sgl_kernel.allreduce import *
     from sgl_kernel.attention import merge_state_v2
     from sgl_kernel.cutlass_moe import (
+        cutlass_mxfp4a8_moe_mm,
         cutlass_w4a8_moe_mm,
         get_cutlass_w4a8_moe_mm_data,
     )
