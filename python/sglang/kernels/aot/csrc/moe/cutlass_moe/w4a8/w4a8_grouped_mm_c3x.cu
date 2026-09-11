2fd0881c66ed61d93c8e50cc42b0c000285805eb
wangyuzhan <wangyuzhan@bytedance.com>
2026-09-12 07:45:54 +0800

    perf(mxfp4a8): pair C64 K512 single warpgroups with drained refill

--- python/sglang/kernels/aot/csrc/moe/cutlass_moe/w4a8/w4a8_grouped_mm_c3x.cu
+++ python/sglang/kernels/aot/csrc/moe/cutlass_moe/w4a8/w4a8_grouped_mm_c3x.cu
@@ -252,6 +252,50 @@ struct SM90_N16_K256_SWG_MXFP4 {
   };
 };
 
+template <int N, int Stages, int Ctas>
+struct SM90_C64_K512_SWG_MXFP4 {
+  using Base = typename SM90_PRECOMPUTED_MXFP4<64, N, 512>::Cutlass3xW4A8Gemm;
+  struct Cutlass3xW4A8Gemm : Base {
+    static constexpr bool UseSingleWarpgroupKernel = true;
+    static constexpr int SingleWarpgroupCtasPerSm = Ctas;
+    using TileShape = cute::Shape<cute::Int<64>, cute::Int<N>, cute::Int<512>>;
+    using OldMainloop = typename Base::CollectiveMainloopScaleOnly;
+    using Policy = cutlass::gemm::MainloopSm90ArrayTmaGmmaWarpSpecializedMixedInputPreScale<
+        Stages, typename OldMainloop::DispatchPolicy::ClusterShape, typename OldMainloop::KernelSchedule>;
+    using CollectiveMainloopScaleOnly = cutlass::gemm::collective::CollectiveMmaArrayMixedInput<
+        Policy,
+        TileShape,
+        cute::tuple<cutlass::float_e2m1_t, cutlass::float_ue8m0_t>,
+        typename OldMainloop::StrideA,
+        cutlass::float_e4m3_t,
+        typename OldMainloop::StrideB,
+        typename OldMainloop::TiledMma,
+        typename OldMainloop::GmemTiledCopyA,
+        typename OldMainloop::SmemLayoutAtomA,
+        typename OldMainloop::SmemCopyAtomA,
+        typename OldMainloop::TransformA,
+        typename OldMainloop::GmemTiledCopyB,
+        typename OldMainloop::SmemLayoutAtomB,
+        typename OldMainloop::SmemCopyAtomB,
+        cutlass::gemm::collective::DrainedK512StageRefill>;
+    using CollectiveEpilogue = typename sgl_kernel::w4a8_detail::W4A8EpilogueSelector<
+        true,
+        true,
+        false,
+        TileShape,
+        cute::Shape<cute::Int<1>, cute::Int<1>, cute::Int<1>>,
+        cutlass::epilogue::PtrArrayTmaWarpSpecializedPingpong>::Type;
+    using GemmKernelScaleOnly = cutlass::gemm::kernel::SingleWarpgroupPersistentGemm<
+        sgl_kernel::w4a8_detail::ProblemShape,
+        CollectiveMainloopScaleOnly,
+        CollectiveEpilogue,
+        Ctas,
+        Stages,
+        cutlass::gemm::kernel::SingleWarpgroupPipelineMode::RollingRefill>;
+    using GemmScaleOnly = cutlass::gemm::device::GemmUniversalAdapter<GemmKernelScaleOnly>;
+  };
+};
+
 template <typename Config>
 inline void invoke_gemm(
     torch::Tensor& d_tensors,
@@ -683,13 +727,22 @@ void dispatch_mxfp4a8_fused_moe_mm_sm90(
           (SM90_TAIL_HANDOFF_MXFP4<SM90_PRECOMPUTED_MXFP4<
               64, 32, 512, 1, 1, false, sgl_kernel::swg_detail::ExpertRowPolicy::Above16>>));
       return;
+    case 445:
+      INVOKE_GEMM_WITH_CONFIG_AS((SM90_C64_K512_SWG_MXFP4<32, 3, 2>));
+      return;
+    case 446:
+      INVOKE_GEMM_WITH_CONFIG_AS((SM90_C64_K512_SWG_MXFP4<32, 2, 3>));
+      return;
+    case 447:
+      INVOKE_GEMM_WITH_CONFIG_AS((SM90_C64_K512_SWG_MXFP4<64, 2, 2>));
+      return;
     default:
       TORCH_CHECK(
           false,
           "Unsupported fused MXFP4A8 config=",
           swg_config,
           "; expected one of 100, 101, 204, 205, 313, 320, 322, 334, 364, 391, 392, 393, 401, 402, 403, 404, 405, "
-          "441");
+          "441, 445, 446, 447");
   }
 }
 
