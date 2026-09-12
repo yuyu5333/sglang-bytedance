2f907cbd61e8636be882c0b1dd142636d76e7825
wangyuzhan <wangyuzhan@bytedance.com>
2026-09-12 18:22:03 +0800

    Revert "perf(mxfp4a8): split N64 main work from short N32 tails"
    This reverts commit d8da4f3e8c4925999e84fdd8c713579c974a0fcc.

--- python/sglang/kernels/aot/csrc/moe/cutlass_moe/w4a8/w4a8_grouped_mm_c3x.cu
+++ python/sglang/kernels/aot/csrc/moe/cutlass_moe/w4a8/w4a8_grouped_mm_c3x.cu
@@ -376,24 +376,6 @@ struct SM90_INDEPENDENT_EPILOGUE_MXFP4 {
   };
 };
 
-template <bool ChunkMajor>
-struct SM90_N64_MAIN_PARTITION_MXFP4 {
-  using Base = SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N64_INDEPENDENT_TMA_MXFP4>::Cutlass3xW4A8Gemm;
-  struct Cutlass3xW4A8Gemm : Base {
-    static constexpr auto ExpertRows = sgl_kernel::swg_detail::ExpertRowPolicy::MainN64;
-    static constexpr bool UseChunkMajorWorkMap = ChunkMajor;
-    using PrecomputedTileScheduler =
-        cutlass::gemm::kernel::detail::PersistentTileSchedulerSm90GroupPrecomputed<
-            sgl_kernel::w4a8_detail::ProblemShape, 8, ChunkMajor>;
-    using GemmKernelScaleOnly = cutlass::gemm::kernel::GemmUniversalPrecomputedScheduler<
-        sgl_kernel::w4a8_detail::ProblemShape,
-        typename Base::CollectiveMainloopScaleOnly,
-        typename Base::CollectiveEpilogue,
-        PrecomputedTileScheduler>;
-    using GemmScaleOnly = cutlass::gemm::device::GemmUniversalAdapter<GemmKernelScaleOnly>;
-  };
-};
-
 template <typename Config>
 inline void invoke_gemm(
     torch::Tensor& d_tensors,
@@ -896,18 +878,6 @@ void dispatch_mxfp4a8_fused_moe_mm_sm90(
       INVOKE_GEMM_WITH_CONFIG_AS(
           (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N16_K256_SWG_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::TailN16>>));
       return;
-    case 508:
-      INVOKE_GEMM_WITH_CONFIG_AS((SM90_N64_MAIN_PARTITION_MXFP4<false>));
-      INVOKE_GEMM_WITH_CONFIG_AS(
-          (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_PRECOMPUTED_MXFP4<
-              128, 32, 512, 1, 1, false, sgl_kernel::swg_detail::ExpertRowPolicy::TailN32>>));
-      return;
-    case 509:
-      INVOKE_GEMM_WITH_CONFIG_AS((SM90_N64_MAIN_PARTITION_MXFP4<true>));
-      INVOKE_GEMM_WITH_CONFIG_AS(
-          (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_PRECOMPUTED_MXFP4<
-              128, 32, 512, 1, 1, false, sgl_kernel::swg_detail::ExpertRowPolicy::TailN32>>));
-      return;
     default:
       TORCH_CHECK(
           false,
@@ -915,7 +885,7 @@ void dispatch_mxfp4a8_fused_moe_mm_sm90(
           swg_config,
           "; expected one of 100, 101, 204, 205, 313, 320, 322, 334, 364, 391, 392, 393, 401, 402, 403, 404, 405, "
           "441, 448, 449, 460, 470, 471, 473, 474, 475, 476, 483, "
-          "503, 506, 507, 508, 509");
+          "503, 506, 507");
   }
 }
 
