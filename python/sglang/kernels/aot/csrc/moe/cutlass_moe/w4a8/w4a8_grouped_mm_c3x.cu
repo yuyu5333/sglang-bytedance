9e3a57534cf1afdaf8b83029976cacd11fbece2f
wangyuzhan <wangyuzhan@bytedance.com>
2026-09-12 23:41:12 +0800

    revert: retain only validated expert-major light ping-pong tactic

--- python/sglang/kernels/aot/csrc/moe/cutlass_moe/w4a8/w4a8_grouped_mm_c3x.cu
+++ python/sglang/kernels/aot/csrc/moe/cutlass_moe/w4a8/w4a8_grouped_mm_c3x.cu
@@ -850,14 +850,6 @@ void dispatch_mxfp4a8_fused_moe_mm_sm90(
       INVOKE_GEMM_WITH_CONFIG_AS(
           (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N16_K256_SWG_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::TailN16>>));
       return;
-    case 517:
-      INVOKE_GEMM_WITH_CONFIG_AS(
-          (SM90_WARP_METADATA_MXFP4<SM90_TAIL_HANDOFF_MXFP4<SM90_PRECOMPUTED_MXFP4<
-              128, 16, 512, 1, 1, true, sgl_kernel::swg_detail::ExpertRowPolicy::AtMost16>>>));
-      INVOKE_GEMM_WITH_CONFIG_AS(
-          (SM90_WARP_METADATA_MXFP4<SM90_TAIL_HANDOFF_MXFP4<SM90_PRECOMPUTED_MXFP4<
-              64, 32, 512, 1, 1, false, sgl_kernel::swg_detail::ExpertRowPolicy::Above16>>>));
-      return;
     case 518:
       INVOKE_GEMM_WITH_CONFIG_AS(
           (SM90_WARP_METADATA_MXFP4<SM90_TAIL_HANDOFF_MXFP4<SM90_PRECOMPUTED_MXFP4<
@@ -872,7 +864,7 @@ void dispatch_mxfp4a8_fused_moe_mm_sm90(
           "Unsupported fused MXFP4A8 config=",
           swg_config,
           "; expected one of 100, 101, 204, 205, 313, 320, 322, 334, 364, 391, 392, 393, 401, 402, 403, 404, 405, "
-          "441, 448, 449, 460, 470, 471, 473, 474, 475, 476, 483, 503, 517, 518");
+          "441, 448, 449, 460, 470, 471, 473, 474, 475, 476, 483, 503, 518");
   }
 }
 
