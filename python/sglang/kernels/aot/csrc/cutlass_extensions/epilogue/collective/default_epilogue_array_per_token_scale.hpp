75ebfd77e5fa53018d7153c956eb1893268c2741
wangyuzhan <wangyuzhan@bytedance.com>
2026-09-14 18:16:29 +0800

    Revert "perf: test K512 N64 packed epilogue with three stages"
    This reverts commit ff5a36c8d366e6ef0654d6e0e76c5b1019b5860a.

--- python/sglang/kernels/aot/csrc/cutlass_extensions/epilogue/collective/default_epilogue_array_per_token_scale.hpp
+++ python/sglang/kernels/aot/csrc/cutlass_extensions/epilogue/collective/default_epilogue_array_per_token_scale.hpp
@@ -386,7 +386,7 @@ class WarpShuffleEpilogueArrayPerTokenScale {
   static constexpr int kOutputAlignment = ElementsPerVector;
 
   static_assert(
-      TileM == 128 && (((TileN == 24 || TileN == 32 || TileN == 64) && TileK == 512) ||
+      TileM == 128 && (((TileN == 24 || TileN == 32) && TileK == 512) ||
                       ((TileN == 40 || TileN == 48 || TileN == 56) && TileK == 256)),
       "The warp-shuffle epilogue requires C128 and a supported N/K pair.");
   static_assert(cute::is_same_v<ElementAccumulator, float>, "The warp-shuffle epilogue requires FP32 accumulators.");
--- python/sglang/kernels/aot/csrc/moe/cutlass_moe/w4a8/w4a8_grouped_mm_c3x.cu
+++ python/sglang/kernels/aot/csrc/moe/cutlass_moe/w4a8/w4a8_grouped_mm_c3x.cu
@@ -352,24 +352,6 @@ struct SM90_LIGHT_K256_PACKED_MXFP4 {
       static_cast<int>(RowPolicy)>;
 };
 
-template <sgl_kernel::swg_detail::ExpertRowPolicy RowPolicy>
-struct SM90_N64_K512_PACKED_MXFP4 {
-  using TileShape = cute::Shape<cute::Int<128>, cute::Int<64>, cute::Int<512>>;
-  using ClusterShape = cute::Shape<cute::Int<1>, cute::Int<1>, cute::Int<1>>;
-  using Cutlass3xW4A8Gemm = cutlass_3x_w4a8_group_gemm<
-      TileShape,
-      ClusterShape,
-      cutlass::gemm::KernelPtrArrayTmaWarpSpecializedPingpong,
-      sgl_kernel::w4a8_detail::WarpShufflePackedStoreGemm2Epilogue,
-      typename QuantTraits<WType::MXFP4>::Element,
-      QuantTraits<WType::MXFP4>::GroupSize,
-      false,
-      true,
-      false,
-      static_cast<int>(RowPolicy)>;
-  static_assert(Cutlass3xW4A8Gemm::CollectiveMainloopScaleOnly::DispatchPolicy::Stages == 3);
-};
-
 template <class BaseConfig, int L2Distance = 0>
 struct SM90_GLOBAL_ACTIVATION_TMA_MXFP4 {
   using Base = typename BaseConfig::Cutlass3xW4A8Gemm;
@@ -1114,19 +1096,6 @@ void dispatch_mxfp4a8_fused_moe_mm_sm90(
       INVOKE_GEMM_WITH_CONFIG_AS(
           (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N16_K256_SWG_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::TailN16>>));
       return;
-    case 613:
-      INVOKE_GEMM_WITH_CONFIG_AS(
-          (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N64_K512_PACKED_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::All>>));
-      return;
-    case 614:
-      INVOKE_GEMM_WITH_CONFIG_AS(
-          (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N64_K512_PACKED_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::MainN64>>));
-      INVOKE_GEMM_WITH_CONFIG_AS(
-          (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_PRECOMPUTED_MXFP4<
-              128, 32, 512, 1, 1, false, sgl_kernel::swg_detail::ExpertRowPolicy::TailN32Of64>>));
-      INVOKE_GEMM_WITH_CONFIG_AS(
-          (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N16_K256_SWG_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::TailN16>>));
-      return;
     default:
       TORCH_CHECK(
           false,
@@ -1134,7 +1103,7 @@ void dispatch_mxfp4a8_fused_moe_mm_sm90(
           swg_config,
           "; expected one of 100, 101, 204, 205, 313, 320, 322, 334, 364, 391, 392, 393, 401, 402, 403, 404, 405, "
           "441, 448, 449, 460, 470, 471, 473, 474, 475, 476, 483, 503, 518, 574, 575, 584, "
-          "603, 604, 605, 606, 607, 608, 609, 610, 611, 612, 613, 614");
+          "603, 604, 605, 606, 607, 608, 609, 610, 611, 612");
   }
 }
 
