0cf3b588848c944dd8c3bfa9b545294127cbd39f
wangyuzhan <wangyuzhan@bytedance.com>
2026-09-14 19:21:58 +0800

    Revert 192-register variants after graph regression
    Partially reverts 5c2ac68bad, preserving 176-register variants for independent seed comparison.

--- python/sglang/kernels/aot/csrc/moe/cutlass_moe/w4a8/w4a8_grouped_mm_c3x.cu
+++ python/sglang/kernels/aot/csrc/moe/cutlass_moe/w4a8/w4a8_grouped_mm_c3x.cu
@@ -1128,21 +1128,6 @@ void dispatch_mxfp4a8_fused_moe_mm_sm90(
       INVOKE_GEMM_WITH_CONFIG_AS(
           (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N16_K256_SWG_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::TailN16>>));
       return;
-    case 624:
-      INVOKE_GEMM_WITH_CONFIG_AS(
-          (SM90_CONSUMER_REGISTERS_MXFP4<
-              SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_PRECOMPUTED_MXFP4<
-                  128, 32, 512, 1, 1, true, sgl_kernel::swg_detail::ExpertRowPolicy::MainN32>, 2>,
-              192>));
-      INVOKE_GEMM_WITH_CONFIG_AS(
-          (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N16_K256_SWG_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::TailN16>>));
-      return;
-    case 625:
-      INVOKE_GEMM_WITH_CONFIG_AS(
-          (SM90_CONSUMER_REGISTERS_MXFP4<SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_PACKED_MAIN_N32_MXFP4>, 192>));
-      INVOKE_GEMM_WITH_CONFIG_AS(
-          (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N16_K256_SWG_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::TailN16>>));
-      return;
     case 626:
       INVOKE_GEMM_WITH_CONFIG_AS(
           (SM90_CONSUMER_REGISTERS_MXFP4<
@@ -1199,7 +1184,7 @@ void dispatch_mxfp4a8_fused_moe_mm_sm90(
           swg_config,
           "; expected one of 100, 101, 204, 205, 313, 320, 322, 334, 364, 391, 392, 393, 401, 402, 403, 404, 405, "
           "441, 448, 449, 460, 470, 471, 473, 474, 475, 476, 483, 503, 518, 574, 575, 584, "
-          "603, 608, 609, 610, 616, 617, 624, 625, 626, 627, 628, 629, 630, 631");
+          "603, 608, 609, 610, 616, 617, 626, 627, 628, 629, 630, 631");
   }
 }
 
