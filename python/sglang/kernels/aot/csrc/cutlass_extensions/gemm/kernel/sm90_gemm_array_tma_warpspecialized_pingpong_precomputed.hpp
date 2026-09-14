8354e2d6fb118f30eda1c0b42310457189024d2f
wangyuzhan <wangyuzhan@bytedance.com>
2026-09-14 19:37:40 +0800

    perf(mxfp4a8): test register redistribution with compact producer

--- python/sglang/kernels/aot/csrc/cutlass_extensions/gemm/kernel/sm90_gemm_array_tma_warpspecialized_pingpong_precomputed.hpp
+++ python/sglang/kernels/aot/csrc/cutlass_extensions/gemm/kernel/sm90_gemm_array_tma_warpspecialized_pingpong_precomputed.hpp
@@ -122,6 +122,13 @@ template <class Mainloop>
 struct CompactProducerWarp<Mainloop, std::void_t<decltype(Mainloop::CompactProducer)>>
     : std::bool_constant<Mainloop::CompactProducer> {};
 
+template <class Mainloop, class = void>
+struct CompactProducerReallocation : std::false_type {};
+
+template <class Mainloop>
+struct CompactProducerReallocation<Mainloop, std::void_t<decltype(Mainloop::ReallocateCompactRegisters)>>
+    : std::bool_constant<Mainloop::ReallocateCompactRegisters> {};
+
 template <class CollectiveMainloop, class = void>
 struct UseTailMmaHandoff {
   static constexpr bool value = false;
@@ -220,6 +227,8 @@ class GemmUniversalPrecomputedScheduler<
   static constexpr uint32_t NumLoadWarpGroups = 1;
   static constexpr uint32_t NumMmaWarpGroups = 2;
   static constexpr bool UseCompactProducer = CompactProducerWarp<CollectiveMainloop>::value;
+  static constexpr bool ReallocateRegisters =
+      !UseCompactProducer || CompactProducerReallocation<CollectiveMainloop>::value;
   static_assert(!UseCompactProducer || cute::size(ClusterShape{}) == 1);
   static_assert(!UseCompactProducer || !UseIndependentTmaProducers<CollectiveMainloop>::value);
   static constexpr uint32_t MaxThreadsPerBlock =
@@ -728,7 +737,7 @@ class GemmUniversalPrecomputedScheduler<
     auto k_tile_count = size<3>(gA_mkl);
 
     if (warp_group_role == WarpGroupRole::Producer) {
-      if constexpr (!UseCompactProducer) {
+      if constexpr (ReallocateRegisters) {
         cutlass::arch::warpgroup_reg_dealloc<LoadRegisterRequirement>();
       }
 
@@ -943,7 +952,7 @@ class GemmUniversalPrecomputedScheduler<
     }  // Producer Warp Group End
 
     else if (warp_group_role == WarpGroupRole::Consumer0 || warp_group_role == WarpGroupRole::Consumer1) {
-      if constexpr (!UseCompactProducer) {
+      if constexpr (ReallocateRegisters) {
         cutlass::arch::warpgroup_reg_alloc<MmaRegisterRequirement>();
       }
 
--- python/sglang/kernels/aot/csrc/moe/cutlass_moe/w4a8/w4a8_grouped_mm_c3x.cu
+++ python/sglang/kernels/aot/csrc/moe/cutlass_moe/w4a8/w4a8_grouped_mm_c3x.cu
@@ -544,12 +544,13 @@ struct SM90_WEIGHT_SWIZZLE_MXFP4 {
   };
 };
 
-template <class BaseConfig>
+template <class BaseConfig, bool Reallocate = false>
 struct SM90_COMPACT_PRODUCER_MXFP4 {
   using Base = typename BaseConfig::Cutlass3xW4A8Gemm;
   struct Cutlass3xW4A8Gemm : Base {
     struct CollectiveMainloopScaleOnly : Base::CollectiveMainloopScaleOnly {
       static constexpr bool CompactProducer = true;
+      static constexpr bool ReallocateCompactRegisters = Reallocate;
     };
     using GemmKernelScaleOnly = cutlass::gemm::kernel::GemmUniversalPrecomputedScheduler<
         sgl_kernel::w4a8_detail::ProblemShape,
@@ -1172,6 +1173,21 @@ void dispatch_mxfp4a8_fused_moe_mm_sm90(
       INVOKE_GEMM_WITH_CONFIG_AS(
           (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N16_K256_SWG_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::TailN16>>));
       return;
+    case 633:
+      INVOKE_GEMM_WITH_CONFIG_AS(
+          (SM90_COMPACT_PRODUCER_MXFP4<
+              SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_PRECOMPUTED_MXFP4<
+                  128, 32, 512, 1, 1, true, sgl_kernel::swg_detail::ExpertRowPolicy::MainN32>, 2>,
+              true>));
+      INVOKE_GEMM_WITH_CONFIG_AS(
+          (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N16_K256_SWG_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::TailN16>>));
+      return;
+    case 634:
+      INVOKE_GEMM_WITH_CONFIG_AS(
+          (SM90_COMPACT_PRODUCER_MXFP4<SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_PACKED_MAIN_N32_MXFP4>, true>));
+      INVOKE_GEMM_WITH_CONFIG_AS(
+          (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N16_K256_SWG_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::TailN16>>));
+      return;
     default:
       TORCH_CHECK(
           false,
@@ -1179,7 +1195,7 @@ void dispatch_mxfp4a8_fused_moe_mm_sm90(
           swg_config,
           "; expected one of 100, 101, 204, 205, 313, 320, 322, 334, 364, 391, 392, 393, 401, 402, 403, 404, 405, "
           "441, 448, 449, 460, 470, 471, 473, 474, 475, 476, 483, 503, 518, 574, 575, 584, "
-          "603, 608, 609, 610, 616, 617, 628, 629, 630, 631, 632");
+          "603, 608, 609, 610, 616, 617, 628, 629, 630, 631, 632, 633, 634");
   }
 }
 
