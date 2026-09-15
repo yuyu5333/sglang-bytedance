7ce8149bb013db2c2865c69b7e7f0c17b44e77c0
wangyuzhan <wangyuzhan@bytedance.com>
2026-09-15 19:29:15 +0800

    perf(mxfp4a8): test full-CTA TMA epilogue tiles

--- python/sglang/kernels/aot/csrc/moe/cutlass_moe/w4a8/w4a8_grouped_mm_c3x.cu
+++ python/sglang/kernels/aot/csrc/moe/cutlass_moe/w4a8/w4a8_grouped_mm_c3x.cu
@@ -525,6 +525,43 @@ struct SM90_SHORT_EPILOGUE_MXFP4 {
   };
 };
 
+template <class BaseConfig>
+struct SM90_FULL_TILE_EPILOGUE_MXFP4 {
+  using Base = typename BaseConfig::Cutlass3xW4A8Gemm;
+  struct Cutlass3xW4A8Gemm : Base {
+    using Mainloop = typename Base::CollectiveMainloopScaleOnly;
+    using Tile = typename Mainloop::TileShape;
+    using EpiTile = cute::Shape<decltype(cute::size<0>(Tile{})), decltype(cute::size<1>(Tile{}))>;
+    using Operation = cutlass::epilogue::fusion::PtrArrayPerTokenScaledAcc<
+        sgl_kernel::w4a8_detail::ElementD, float, float>;
+    using CollectiveEpilogue =
+        typename tensorrt_llm::cutlass_extensions::epilogue::collective::MixedInputSm90TmaEpilogueBuilder<
+            cutlass::arch::Sm90,
+            cutlass::arch::OpClassTensorOp,
+            Tile,
+            typename Mainloop::DispatchPolicy::ClusterShape,
+            EpiTile,
+            float,
+            float,
+            sgl_kernel::w4a8_detail::ElementC,
+            sgl_kernel::w4a8_detail::LayoutC_Transpose*,
+            8,
+            sgl_kernel::w4a8_detail::ElementD,
+            sgl_kernel::w4a8_detail::LayoutD_Transpose*,
+            8,
+            cutlass::epilogue::PtrArrayTmaWarpSpecializedPingpong,
+            Operation>::CollectiveOp;
+    using GemmKernelScaleOnly = cutlass::gemm::kernel::GemmUniversalPrecomputedScheduler<
+        sgl_kernel::w4a8_detail::ProblemShape,
+        Mainloop,
+        CollectiveEpilogue,
+        typename Base::PrecomputedTileScheduler>;
+    static_assert(sizeof(typename GemmKernelScaleOnly::SharedStorage) + 1024 <=
+                  228 * 1024 / GemmKernelScaleOnly::MinBlocksPerMultiprocessor);
+    using GemmScaleOnly = cutlass::gemm::device::GemmUniversalAdapter<GemmKernelScaleOnly>;
+  };
+};
+
 template <typename Config>
 inline void invoke_gemm(
     torch::Tensor& d_tensors,
@@ -1086,6 +1123,26 @@ void dispatch_mxfp4a8_fused_moe_mm_sm90(
       INVOKE_GEMM_WITH_CONFIG_AS(
           (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N16_K256_SWG_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::TailN16>>));
       return;
+    case 646:
+      INVOKE_GEMM_WITH_CONFIG_AS(
+          (SM90_FULL_TILE_EPILOGUE_MXFP4<
+              SM90_WEIGHT_SWIZZLE_MXFP4<
+                  SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_PRECOMPUTED_MXFP4<
+                      128, 32, 512, 1, 1, true, sgl_kernel::swg_detail::ExpertRowPolicy::MainN32>, 2>,
+                  64>>));
+      INVOKE_GEMM_WITH_CONFIG_AS(
+          (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N16_K256_SWG_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::TailN16>>));
+      return;
+    case 647:
+      INVOKE_GEMM_WITH_CONFIG_AS(
+          (SM90_FULL_TILE_EPILOGUE_MXFP4<
+              SM90_TWO_CTA_PINGPONG_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::MainN64>>));
+      INVOKE_GEMM_WITH_CONFIG_AS(
+          (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_PRECOMPUTED_MXFP4<
+              128, 32, 512, 1, 1, false, sgl_kernel::swg_detail::ExpertRowPolicy::TailN32Of64>>));
+      INVOKE_GEMM_WITH_CONFIG_AS(
+          (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N16_K256_SWG_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::TailN16>>));
+      return;
     default:
       TORCH_CHECK(
           false,
@@ -1093,7 +1150,7 @@ void dispatch_mxfp4a8_fused_moe_mm_sm90(
           swg_config,
           "; expected one of 100, 101, 204, 205, 313, 320, 322, 334, 364, 391, 392, 393, 401, 402, 403, 404, 405, "
           "441, 448, 449, 460, 470, 471, 473, 474, 475, 476, 483, 503, 518, 574, 575, 584, "
-          "608, 616, 643, 644, 645");
+          "608, 616, 643, 644, 645, 646, 647");
   }
 }
 
