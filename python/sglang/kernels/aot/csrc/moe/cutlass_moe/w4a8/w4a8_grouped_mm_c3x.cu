9542a5e848957ae37ea479d17a62b681525f9282
wangyuzhan <wangyuzhan@bytedance.com>
2026-09-15 18:38:17 +0800

    perf(mxfp4a8): share activation stages across channel consumers

--- python/sglang/kernels/aot/csrc/moe/cutlass_moe/w4a8/w4a8_grouped_mm_c3x.cu
+++ python/sglang/kernels/aot/csrc/moe/cutlass_moe/w4a8/w4a8_grouped_mm_c3x.cu
@@ -9,6 +9,7 @@
 
 #include "cutlass/cutlass.h"
 #include "w4a8_grouped_mm_c3x.cuh"
+#include "w4a8_channel_cooperative.cuh"
 
 using namespace cute;
 using sgl_kernel::w4a8_detail::cutlass_3x_w4a8_group_gemm;
@@ -481,6 +482,43 @@ struct SM90_WEIGHT_SWIZZLE_MXFP4 {
         sgl_kernel::w4a8_detail::ProblemShape,
         CollectiveMainloopScaleOnly,
         typename Base::CollectiveEpilogue,
+        typename Base::PrecomputedTileScheduler>;
+    using GemmScaleOnly = cutlass::gemm::device::GemmUniversalAdapter<GemmKernelScaleOnly>;
+  };
+};
+
+struct SM90_CHANNEL_COOPERATIVE_MXFP4 {
+  using Base = SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_PRECOMPUTED_MXFP4<128, 32, 256>>::Cutlass3xW4A8Gemm;
+  struct Cutlass3xW4A8Gemm : Base {
+    static constexpr auto ExpertRows = sgl_kernel::swg_detail::ExpertRowPolicy::MainN32;
+    static constexpr bool UseWarpShuffleGemm2Epilogue = true;
+    using OldMainloop = typename Base::CollectiveMainloopScaleOnly;
+    using Policy = cutlass::gemm::MainloopSm90ArrayTmaGmmaWarpSpecializedMixedInputPreScale<
+        3, typename OldMainloop::DispatchPolicy::ClusterShape, typename OldMainloop::KernelSchedule>;
+    using CollectiveMainloopScaleOnly = cutlass::gemm::collective::CollectiveMmaArrayMixedInput<
+        Policy,
+        typename OldMainloop::TileShape,
+        cute::tuple<cutlass::float_e2m1_t, cutlass::float_ue8m0_t>,
+        typename OldMainloop::StrideA,
+        cutlass::float_e4m3_t,
+        typename OldMainloop::StrideB,
+        typename OldMainloop::TiledMma,
+        typename OldMainloop::GmemTiledCopyA,
+        typename OldMainloop::SmemLayoutAtomA,
+        typename OldMainloop::SmemCopyAtomA,
+        typename OldMainloop::TransformA,
+        typename OldMainloop::GmemTiledCopyB,
+        typename OldMainloop::SmemLayoutAtomB,
+        typename OldMainloop::SmemCopyAtomB,
+        typename OldMainloop::TransformB>;
+    using CollectiveEpilogue = typename sgl_kernel::w4a8_detail::W4A8EpilogueSelector<
+        true, true, false, typename OldMainloop::TileShape,
+        typename OldMainloop::DispatchPolicy::ClusterShape,
+        cutlass::epilogue::PtrArrayNoSmemWarpSpecialized>::Type;
+    using GemmKernelScaleOnly = sgl_kernel::w4a8_detail::ChannelCooperativeKernel<
+        sgl_kernel::w4a8_detail::ProblemShape,
+        CollectiveMainloopScaleOnly,
+        CollectiveEpilogue,
         typename Base::PrecomputedTileScheduler>;
     using GemmScaleOnly = cutlass::gemm::device::GemmUniversalAdapter<GemmKernelScaleOnly>;
   };
@@ -1024,6 +1062,11 @@ void dispatch_mxfp4a8_fused_moe_mm_sm90(
       INVOKE_GEMM_WITH_CONFIG_AS(
           (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N16_K256_SWG_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::TailN16>>));
       return;
+    case 640:
+      INVOKE_GEMM_WITH_CONFIG_AS((SM90_CHANNEL_COOPERATIVE_MXFP4));
+      INVOKE_GEMM_WITH_CONFIG_AS(
+          (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N16_K256_SWG_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::TailN16>>));
+      return;
     default:
       TORCH_CHECK(
           false,
@@ -1031,7 +1074,7 @@ void dispatch_mxfp4a8_fused_moe_mm_sm90(
           swg_config,
           "; expected one of 100, 101, 204, 205, 313, 320, 322, 334, 364, 391, 392, 393, 401, 402, 403, 404, 405, "
           "441, 448, 449, 460, 470, 471, 473, 474, 475, 476, 483, 503, 518, 574, 575, 584, "
-          "608, 616");
+          "608, 616, 640");
   }
 }
 
