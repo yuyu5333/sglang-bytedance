6ffc2e8b7a6a3983077034926969b5844055a073
wangyuzhan <wangyuzhan@bytedance.com>
2026-09-15 13:42:59 +0800

    Revert weight swizzle variants without stable incremental benefit
    Partial revert of 6af7eadac1: retain only verified GEMM1 616 and remove 617 plus unreachable SW32 layout branch.

--- python/sglang/kernels/aot/csrc/moe/cutlass_moe/w4a8/w4a8_grouped_mm_c3x.cu
+++ python/sglang/kernels/aot/csrc/moe/cutlass_moe/w4a8/w4a8_grouped_mm_c3x.cu
@@ -456,11 +456,8 @@ struct SM90_WEIGHT_SWIZZLE_MXFP4 {
   struct Cutlass3xW4A8Gemm : Base {
     using OldMainloop = typename Base::CollectiveMainloopScaleOnly;
     static_assert(!OldMainloop::SwapAB);
-    static_assert(Bytes == 32 || Bytes == 64);
-    using WeightLayout = std::conditional_t<
-        Bytes == 32,
-        cute::GMMA::Layout_K_SW32_Atom<cutlass::float_e2m1_t>,
-        cute::GMMA::Layout_K_SW64_Atom<cutlass::float_e2m1_t>>;
+    static_assert(Bytes == 64);
+    using WeightLayout = cute::GMMA::Layout_K_SW64_Atom<cutlass::float_e2m1_t>;
     static_assert(!std::is_same_v<WeightLayout, typename OldMainloop::SmemLayoutAtomA>);
     using Mainloop = cutlass::gemm::collective::CollectiveMmaArrayMixedInput<
         typename OldMainloop::DispatchPolicy,
@@ -1061,12 +1058,6 @@ void dispatch_mxfp4a8_fused_moe_mm_sm90(
       INVOKE_GEMM_WITH_CONFIG_AS(
           (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N16_K256_SWG_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::TailN16>>));
       return;
-    case 617:
-      INVOKE_GEMM_WITH_CONFIG_AS(
-          (SM90_WEIGHT_SWIZZLE_MXFP4<SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_PACKED_MAIN_N32_MXFP4>, 64>));
-      INVOKE_GEMM_WITH_CONFIG_AS(
-          (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N16_K256_SWG_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::TailN16>>));
-      return;
     case 636:
       INVOKE_GEMM_WITH_CONFIG_AS((SM90_DUAL_PANEL_DECODE_MXFP4<128, 2, 2>));
       INVOKE_GEMM_WITH_CONFIG_AS(
@@ -1082,7 +1073,7 @@ void dispatch_mxfp4a8_fused_moe_mm_sm90(
           swg_config,
           "; expected one of 100, 101, 204, 205, 313, 320, 322, 334, 364, 391, 392, 393, 401, 402, 403, 404, 405, "
           "441, 448, 449, 460, 470, 471, 473, 474, 475, 476, 483, 503, 518, 574, 575, 584, "
-          "608, 616, 617, 636");
+          "608, 616, 636");
   }
 }
 
