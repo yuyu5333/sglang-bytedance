4a2ddd0ca4eb943ee7be5ef7fa1e8af61a5fddc3
wangyuzhan <wangyuzhan@bytedance.com>
2026-09-15 18:28:39 +0800

    Revert pending warp-MMA prototype from acceptance source
    Temporarily reverts c25671691f to freeze verified 608/616 dispatch for r12. Independent candidate build and correctness evaluation remain pending; this is not a performance rejection.

--- python/sglang/kernels/aot/csrc/moe/cutlass_moe/w4a8/w4a8_grouped_mm_c3x.cu
+++ python/sglang/kernels/aot/csrc/moe/cutlass_moe/w4a8/w4a8_grouped_mm_c3x.cu
@@ -9,7 +9,6 @@
 
 #include "cutlass/cutlass.h"
 #include "w4a8_grouped_mm_c3x.cuh"
-#include "w4a8_warp_mma.cuh"
 
 using namespace cute;
 using sgl_kernel::w4a8_detail::cutlass_3x_w4a8_group_gemm;
@@ -483,40 +482,6 @@ struct SM90_WEIGHT_SWIZZLE_MXFP4 {
         CollectiveMainloopScaleOnly,
         typename Base::CollectiveEpilogue,
         typename Base::PrecomputedTileScheduler>;
-    using GemmScaleOnly = cutlass::gemm::device::GemmUniversalAdapter<GemmKernelScaleOnly>;
-  };
-};
-
-struct SM90_WARP_MMA_MXFP4 {
-  using Base = SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_PRECOMPUTED_MXFP4<128, 32, 256>>::Cutlass3xW4A8Gemm;
-  struct Cutlass3xW4A8Gemm : Base {
-    static constexpr auto ExpertRows = sgl_kernel::swg_detail::ExpertRowPolicy::MainN32;
-    using OldMainloop = typename Base::CollectiveMainloopScaleOnly;
-    using Policy = cutlass::gemm::MainloopSm90ArrayTmaGmmaWarpSpecializedMixedInputPreScale<
-        3, typename OldMainloop::DispatchPolicy::ClusterShape, typename OldMainloop::KernelSchedule>;
-    using RawMainloop = cutlass::gemm::collective::CollectiveMmaArrayMixedInput<
-        Policy,
-        typename OldMainloop::TileShape,
-        cute::tuple<cutlass::float_e2m1_t, cutlass::float_ue8m0_t>,
-        typename OldMainloop::StrideA,
-        cutlass::float_e4m3_t,
-        typename OldMainloop::StrideB,
-        typename OldMainloop::TiledMma,
-        typename OldMainloop::GmemTiledCopyA,
-        typename OldMainloop::SmemLayoutAtomA,
-        typename OldMainloop::SmemCopyAtomA,
-        typename OldMainloop::TransformA,
-        typename OldMainloop::GmemTiledCopyB,
-        typename OldMainloop::SmemLayoutAtomB,
-        typename OldMainloop::SmemCopyAtomB,
-        typename OldMainloop::TransformB>;
-    using CollectiveMainloopScaleOnly = sgl_kernel::w4a8_detail::WarpMmaMainloop<RawMainloop>;
-    using GemmKernelScaleOnly = cutlass::gemm::kernel::GemmUniversalPrecomputedScheduler<
-        sgl_kernel::w4a8_detail::ProblemShape,
-        CollectiveMainloopScaleOnly,
-        typename Base::CollectiveEpilogue,
-        typename Base::PrecomputedTileScheduler>;
-    static_assert(sizeof(typename GemmKernelScaleOnly::SharedStorage) <= 114 * 1024);
     using GemmScaleOnly = cutlass::gemm::device::GemmUniversalAdapter<GemmKernelScaleOnly>;
   };
 };
@@ -1059,11 +1024,6 @@ void dispatch_mxfp4a8_fused_moe_mm_sm90(
       INVOKE_GEMM_WITH_CONFIG_AS(
           (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N16_K256_SWG_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::TailN16>>));
       return;
-    case 639:
-      INVOKE_GEMM_WITH_CONFIG_AS((SM90_WARP_MMA_MXFP4));
-      INVOKE_GEMM_WITH_CONFIG_AS(
-          (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N16_K256_SWG_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::TailN16>>));
-      return;
     default:
       TORCH_CHECK(
           false,
@@ -1071,7 +1031,7 @@ void dispatch_mxfp4a8_fused_moe_mm_sm90(
           swg_config,
           "; expected one of 100, 101, 204, 205, 313, 320, 322, 334, 364, 391, 392, 393, 401, 402, 403, 404, 405, "
           "441, 448, 449, 460, 470, 471, 473, 474, 475, 476, 483, 503, 518, 574, 575, 584, "
-          "608, 616, 639");
+          "608, 616");
   }
 }
 
