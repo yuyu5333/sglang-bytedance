de5117736476f55f8eb898fea2f4665103c9cfb8
wangyuzhan <wangyuzhan@bytedance.com>
2026-09-16 15:35:45 +0800

    Revert unsupported C128 full-tile epilogue
    Partially revert 7ce8149bb0. The mixed-input epilogue indexes one M64 accumulator block per output subtile. Keep the supported C64/N64 candidate 647 and reject C128 overrides at compile time.

--- python/sglang/kernels/aot/csrc/moe/cutlass_moe/w4a8/w4a8_grouped_mm_c3x.cu
+++ python/sglang/kernels/aot/csrc/moe/cutlass_moe/w4a8/w4a8_grouped_mm_c3x.cu
@@ -531,6 +531,7 @@ struct SM90_FULL_TILE_EPILOGUE_MXFP4 {
   struct Cutlass3xW4A8Gemm : Base {
     using Mainloop = typename Base::CollectiveMainloopScaleOnly;
     using Tile = typename Mainloop::TileShape;
+    static_assert(cute::size<0>(Tile{}) == 64);
     using EpiTile = cute::Shape<decltype(cute::size<0>(Tile{})), decltype(cute::size<1>(Tile{}))>;
     using Operation = cutlass::epilogue::fusion::PtrArrayPerTokenScaledAcc<
         sgl_kernel::w4a8_detail::ElementD, float, float>;
@@ -1110,16 +1111,6 @@ void dispatch_mxfp4a8_fused_moe_mm_sm90(
       INVOKE_GEMM_WITH_CONFIG_AS(
           (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N16_K256_SWG_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::TailN16>>));
       return;
-    case 646:
-      INVOKE_GEMM_WITH_CONFIG_AS(
-          (SM90_FULL_TILE_EPILOGUE_MXFP4<
-              SM90_WEIGHT_SWIZZLE_MXFP4<
-                  SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_PRECOMPUTED_MXFP4<
-                      128, 32, 512, 1, 1, true, sgl_kernel::swg_detail::ExpertRowPolicy::MainN32>, 2>,
-                  64>>));
-      INVOKE_GEMM_WITH_CONFIG_AS(
-          (SM90_GLOBAL_ACTIVATION_TMA_MXFP4<SM90_N16_K256_SWG_MXFP4<sgl_kernel::swg_detail::ExpertRowPolicy::TailN16>>));
-      return;
     case 647:
       INVOKE_GEMM_WITH_CONFIG_AS(
           (SM90_FULL_TILE_EPILOGUE_MXFP4<
@@ -1137,7 +1128,7 @@ void dispatch_mxfp4a8_fused_moe_mm_sm90(
           swg_config,
           "; expected one of 100, 101, 204, 205, 313, 320, 322, 334, 364, 391, 392, 393, 401, 402, 403, 404, 405, "
           "441, 448, 449, 460, 470, 471, 473, 474, 475, 476, 483, 503, 518, 574, 575, 584, "
-          "608, 616, 645, 646, 647");
+          "608, 616, 645, 647");
   }
 }
 
